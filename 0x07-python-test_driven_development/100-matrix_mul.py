d47659b3c75329e08eddf18513547ce082ae3d9c
Mike Attara <mpyebattara@gmail.com>
2023-10-04 18:28:19 -0700

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy."""
-import numpy as np
+"""Defines a matrix multiplication function."""
 
 
-def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
+def matrix_mul(m_a, m_b):
+    """Multiply two matrices.
 
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-    """
-
-    return (np.matmul(m_a, m_b))
+    Raises:
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
