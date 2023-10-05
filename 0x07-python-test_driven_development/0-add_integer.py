7bda6d80a6733b07d6385677a55a38e2a5d2d835
Mike Attara <mpyebattara@gmail.com>
2023-10-04 17:45:32 -0700

    integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+
+This module is composed by a function that adds two numbers
+
+"""
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
